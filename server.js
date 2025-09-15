ce81f5be888a395dfc19a0d799a53328fab99d26
Lenaavn <leandro1531@outlook.es>
2025-09-15 18:30:15 +0200

    cx

--- server.js
+++ server.js
@@ -4,14 +4,18 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
 
+// Definir la ruta al build de Angular
+const distFolder = path.join(__dirname, 'dist/rehobot_front/browser');
+
 // Servir archivos estáticos
-app.use(express.static(path.join(__dirname, 'dist/rehobot_front/browser')));
+app.use(express.static(distFolder));
 
 // Redirigir todas las rutas al index.html
 app.get(/.*/, (req, res) => {
   res.sendFile(path.join(distFolder, 'index.html'));
 });
 
+// Iniciar el servidor
 app.listen(port, () => {
-  console.log(`Frontend corriendo en http://localhost:${port}`);
+  console.log(`🚀 Frontend corriendo en http://localhost:${port}`);
 });
